019eddb01df4b523e06381481e3afaa4a65358ea
游翰灵 <2632879190@qq.com>
2026-06-03 19:44:03 +0800

    Create server.h

---
+++ llfc/手写版/demo base/server.h
@@ -0,0 +1,65 @@
+#pragma once
+#include <iostream>
+#include <string>
+#include <unordered_map>
+#include <boost/asio.hpp>
+#include <deque>
+#include <string_view>
+// uuid algorithm
+#include <boost/uuid/uuid.hpp>
+#include <boost/uuid/uuid_generators.hpp>
+#include <boost/uuid/uuid_io.hpp>
+#include <thread>
+#include <chrono>
+using boost::asio::ip::tcp;
+
+namespace protocol {
+	using msg_len_t = uint16_t;
+	constexpr msg_len_t MAX_BODY_LENGTH = 8192;
+	constexpr size_t HEADER_SIZE = sizeof(msg_len_t);
+}
+
+class Server: public std::enable_shared_from_this<Server>
+{
+public:
+	Server(boost::asio::io_context& ioc, unsigned short port);
+	~Server() = default;
+
+	void Start();
+	void Stop();
+	void RemoveSession(const std::string& uuid);
+
+private:
+	void DoAccept();
+	
+	std::unordered_map<std::string, std::shared_ptr<Session>> _sessions;
+	boost::asio::ip::tcp::acceptor _acceptor;
+	boost::asio::io_context& _ioc;
+};
+
+
+class Session : public std::enable_shared_from_this<Session> {
+public:
+	Session(tcp::socket socket, std::weak_ptr<Server> server);
+	~Session() = default;
+
+	void Start();
+	void Close(const std::string& reason, const boost::system::error_code& ec);
+	void Send(std::string msg);
+	const std::string& GetUuid() const;
+
+private:
+	void DoWrite();
+	void DoRead();
+
+	std::weak_ptr<Server> _server;
+	std::string _uuid;
+	tcp::socket _socket;
+
+	std::array<char, 4096> _recv_buffer{};
+	//Send queue and status
+	std::deque<std::string> _send_queue;
+	bool _is_writing = false;
+	bool _is_closed = false;
+};
+

93594a13cb100a768b8e654c661a0b6fdb48af75
游翰灵 <2632879190@qq.com>
2026-06-03 19:44:24 +0800

    Create server.cpp

---
+++ llfc/手写版/demo base/server.cpp
@@ -0,0 +1,134 @@
+#include "Server.h"
+
+//=========================================Server===============================================
+Server::Server(boost::asio::io_context& ioc, unsigned short port) : _acceptor(ioc, tcp::endpoint(tcp::v4(), port)), _ioc(ioc) {}
+
+void Server::Start() {
+	DoAccept();
+}
+
+void Server::RemoveSession(const std::string& uuid) {
+	_sessions.erase(uuid);
+	std::cout << "[Server] remove session: " << uuid << "\n";
+}
+
+void Server::Stop(){
+	boost::system::error_code ec;
+	_acceptor.close();
+
+	auto sessions_to_close = std::move(_sessions);
+	for (auto& [uuid, session] : sessions_to_close) {
+		session->Close("Server Close", ec);
+	}
+}
+
+void Server::DoAccept() {
+	_acceptor.async_accept(
+		[this, self = shared_from_this()](boost::system::error_code ec, tcp::socket socket) {
+			if (!ec) {
+				auto session = std::make_shared<Session>(std::move(socket), weak_from_this());
+				session->Start();
+				_sessions[session->GetUuid()] = session;
+				std::cout << "[Server] new connection: " << session->GetUuid() << "\n";
+				DoAccept();
+			}
+			else if (ec == boost::asio::error::operation_aborted) {
+				std::cout << "[Server] Acceptor stopped.\n";
+			}
+			else {
+				std::cerr << "[Server] Accept error" << ec.message() << "\n";
+				DoAccept();
+			}
+		}
+	);
+}
+//===========================================Session================================
+
+Session::Session(tcp::socket socket, std::weak_ptr<Server> server) : _socket(std::move(socket)), _server(server) {
+	static thread_local boost::uuids::random_generator gen;
+	_uuid = boost::uuids::to_string(gen());
+}
+
+const std::string& Session::GetUuid() const { return _uuid; }
+
+void Session::Start() {
+	DoRead();
+}
+
+void Session::Send(std::string msg) {
+	boost::asio::post(
+		_socket.get_executor(),
+		[this, self = shared_from_this(), data = std::move(msg)]() mutable {
+			if (_is_closed) return; //防御 1：任务入队之前，确认socket打开，丢弃新数据
+			bool write_in_progress = _is_writing;
+			_send_queue.push_back(std::move(data));
+
+			if (!write_in_progress) {
+				_is_writing = true;
+				DoWrite();
+			}
+		}
+	);
+}
+
+void Session::DoWrite() {
+	//防御 2: 在发起异步写之前，再次确认socket打开的，拒绝发起新I/O 
+	if (!_socket.is_open() || _is_closed) {
+		_is_writing = false;
+		return;
+	}
+	boost::asio::async_write(
+		_socket,
+		boost::asio::buffer(_send_queue.front()),
+		[this, self = shared_from_this()](boost::system::error_code ec, size_t bytes_transferrd) {
+			if (!ec) {
+				_send_queue.pop_front();
+				if (!_send_queue.empty()) {
+					DoWrite();
+				}
+				else {
+					_is_writing = false;
+				}
+			}
+			else {
+				Close("Write", ec);
+			}
+		}
+	);
+}
+
+void Session::DoRead() {
+	_socket.async_read_some(
+		boost::asio::buffer(_recv_buffer),
+		[this, self = shared_from_this()](boost::system::error_code ec, size_t bytes_transferrd) {
+			if (!ec) {
+				std::string_view msg_view(_recv_buffer.data(), bytes_transferrd);
+				std::cout << "[Session " << _uuid << "] Receive: " << msg_view << "\n";
+				Send(std::string(msg_view));
+				DoRead();
+			}
+			else {
+				Close("Read", ec);
+			}
+		}
+	);
+}
+
+void Session::Close(const std::string& reason, const boost::system::error_code& ec) {
+	// 状态检查：如果已经关闭过，直接返回，拦截所有重复回调
+	if (_is_closed) return;
+	_is_closed = true;
+
+	if (ec) {
+		std::cerr << "[Session " << _uuid << "] " << reason << " Error: " << ec.message() << "\n";
+	}
+	else {
+		std::cout << "[Session " << _uuid << "] Closed normally.\n";
+	}
+	boost::system::error_code ignore_ec;
+	_socket.close(ignore_ec);
+
+	if (auto server = _server.lock()) {
+		server->RemoveSession(_uuid);
+	}
+}

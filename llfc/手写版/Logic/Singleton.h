93824fbad3b752adf3f469639c9ac7a9be1ea01a
游翰灵 <2632879190@qq.com>
2026-06-17 20:01:14 +0800

    Update Singleton.h

--- llfc/手写版/Logic/Singleton.h
+++ llfc/手写版/Logic/Singleton.h
@@ -8,18 +8,12 @@ class Singleton {
 protected:
 	Singleton() = default;
 	~Singleton() = default;
-
 	Singleton(const Singleton<T>&) = delete;
 	Singleton operator=(const Singleton<T>&) = delete;
 
-	static std::shared_ptr<T> _instance;
-
 public:
-	static std::shared_ptr<T> GetInstance() {
-		static std::once_flag s_flag;
-		std::call_once(s_flag, [&]() {
-			_instance = std::shared_ptr<T>(new T);
-		});
+	static T& GetInstance() {
+		static T _instance;
 		return _instance;
 	}
 
@@ -27,7 +21,3 @@ public:
 		std::cout << "Singleton address: " << _instance.get(); << "\n";
 	}
 };
-
-// 静态成员初始化
-template <typename T>
-std::shared_ptr<T> Singleton<T>::_instance = nullptr;
